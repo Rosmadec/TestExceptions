24c1f1e0f6c26ea31a90a6c94fdabfc1e14f03ef
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-05-05 18:47:44 +0200

    Initial commit

---
+++ src/exceptions/testException.java
@@ -0,0 +1,34 @@
+package exceptions;
+
+public class testException {
+
+    public static void method3(){
+        System.out.println("BEGIN method3");
+        int value = (int) (Math.random()*3);
+        int res = 1/value;
+        System.out.println("Res == " + res);
+        System.out.println("END method3");
+    }
+
+    public static void method2(){
+        System.out.println("BEGIN method2");
+        method3();
+        System.out.println("END method2");
+    }
+
+    public static void method1(){
+        System.out.println("BEGIN method1");
+        method2();
+        System.out.println("END method1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("BEGIN main");
+        method1();
+        System.out.println("END main");
+    }
+
+
+
+
+}
