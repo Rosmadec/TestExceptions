a4de3bbb6f65b7f6dd49adeda8f0c9484e122a71
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-05-06 17:45:38 +0200

    ok

--- src/exceptions/TestException.java
+++ src/exceptions/TestException.java
@@ -1,17 +1,40 @@
 package exceptions;
 
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class TestException {
+
+    public static void method4(){
+        System.out.println("BEGIN method4");
+        try {
+            FileOutputStream fos = new FileOutputStream("save.data");
+            fos.write("Ok\n".getBytes());
+            fos.close();
+        }catch (Exception exception){
+            System.out.println("Problème d'écriture fichier. Voulez-vous recommencer (Y/N) : ");
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
+        }
+        System.out.println("END method4");
+    }
+
     public static void method3(){
         System.out.println("BEGIN method3");
         int value = (int) (Math.random()*3);
-        int res = 1/value;
-        System.out.println("Res == " + res);
+        if (value == 0){
+            throw new RuntimeException("Value cannot be null");
+        } else {
+            int res = 1 / value;
+            System.out.println("Res == " + res);
+        }
         System.out.println("END method3");
     }
 
     public static void method2(){
         System.out.println("BEGIN method2");
-        method3();
+        method4();
         System.out.println("END method2");
     }
 
