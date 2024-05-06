ac967f81162dc1e5ecd7181bbe5e6fb2cb48f419
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-05-06 21:39:54 +0200

    ok

--- src/exceptions/RationalStart.java
+++ src/exceptions/RationalStart.java
@@ -1,4 +1,22 @@
 package exceptions;
 
 public class RationalStart {
+    public static void main(String[] args) {
+
+        try {
+            Rational r1 = new Rational(1, 3);
+            Rational r2 = new Rational(1, 0);
+
+            System.out.println(r1.add(r2));
+
+        } catch (RationalException exception){
+            System.out.println("Problème rencontré");
+        }
+
+
+
+
+
+    }
+
 }
