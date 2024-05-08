12de7b124d7506bbd17cba720b617c4c13236096
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-05-08 11:15:15 +0200

    ok

--- src/exceptions/AdvancedException.java
+++ src/exceptions/AdvancedException.java
@@ -12,7 +12,7 @@ public class AdvancedException {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Webstore", "login", "pass");
              FileOutputStream fos = new FileOutputStream("save.xml")) {
 
-        } catch (SQLException | IOException exception) {
+        } catch (SQLException | IOException exception) { // multi catch possible depuis Java 7
             System.out.println("In my catch block");
             exception.printStackTrace();
         /*}catch (SQLException exception){
@@ -22,8 +22,25 @@ public class AdvancedException {
         }
     }
 
+    public static void exportData2() throws SQLException, IOException{
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Webstore", "login", "pass");
+             FileOutputStream fos = new FileOutputStream("save.xml")) {
+
+        } catch (Exception exception) { // multi catch possible depuis Java 7
+            System.out.println("Partial handling - in my catch block");
+            throw exception;
+        }
+    }
+
     public static void main(String[] args) {
-        exportData();
+        //exportData();
+        try {
+            exportData2();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            System.out.println("End handling");
+        }
+
     }
 
 
