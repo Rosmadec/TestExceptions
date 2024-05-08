f2cf5aaa83badc7c5668e35c2d6d134db1ecb596
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-05-08 10:52:33 +0200

    ok

--- src/exceptions/FileWriter.java
+++ src/exceptions/FileWriter.java
@@ -3,6 +3,7 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.*;
 
 public class FileWriter {
 
@@ -41,7 +42,22 @@ public class FileWriter {
         }
 
         System.out.println("Ecriture terminée");
+    }
 
+    // Exemple avec une connection à une BDD
+    // Les classes Connection, PreparedStatement et ResultSet sont autoclosable
+    public void connectToDataBase(){
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/Webstore", "login", "pass")){
+            String sql = "Select * from T_Users where login = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.setString(1, "bond");
+                try (ResultSet resultSet = statement.executeQuery()){
+                    // TODO
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
